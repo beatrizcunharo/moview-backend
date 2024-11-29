5951df53173b97775058b3d479f5e5fd2628e1b9
beatrizcunharo <beatrizcunharo@gmail.com>
2024-11-28 22:02:47 -0300

    feat: Adding cors

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const routes = require('./api/routes')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 8000
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 
 app.use('/api', routes)
 
